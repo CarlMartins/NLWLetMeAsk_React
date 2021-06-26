f79a8bf20d3808cf562b8a35997eb50b5f387a49
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-06-26 15:00:05 -0300

    Criado hook de autenticacao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,11 @@ import { Button } from '../components/Button';
 
 
 import '../styles/auth.scss';
-import { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home () {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom () {
     if (!user) {
