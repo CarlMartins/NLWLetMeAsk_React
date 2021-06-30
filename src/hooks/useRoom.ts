6352162e793e1dd7c7489b8b306629da34898952
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-06-30 18:00:39 -0300

    Primeira etapa criacao sala admin

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -27,7 +27,7 @@ export function useRoom (roomId: string) {
   const [title, setTitle] = useState('');
 
   useEffect(() => {
-    const roomRef = database.ref(`rooms/${ roomId }`);
+    const roomRef = database.ref(`rooms/${roomId}`);
 
     roomRef.on('value', room => {
       const databaseRoom = room.val();
