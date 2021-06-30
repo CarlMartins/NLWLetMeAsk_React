5c9920985099047c0311ba581e62f19d9b84f721
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-06-30 19:30:27 -0300

    Implementacao inicial sistema de likes

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import '../styles/question.scss';
 
 type QuestionProps = {
@@ -6,12 +8,14 @@ type QuestionProps = {
     name: string;
     avatar: string;
   };
+  children?: ReactNode;
 };
 
 
 export function Question ({
   content,
-  author
+  author,
+  children
 }: QuestionProps) {
   return (
     <div className="question">
@@ -21,7 +25,9 @@ export function Question ({
           <img src={ author.avatar } alt={ author.name } />
           <span>{ author.name }</span>
         </div>
-        <div></div>
+        <div>
+          { children }
+        </div>
       </footer>
     </div>
   );
