61bdf1a556b7e7b01f74bcb866c60e226d157a1f
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-07-01 14:27:47 -0300

    Criadas funcionalidades de destaque e pergunta respondida

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import classNames from 'classnames';
 
 import '../styles/question.scss';
 
@@ -9,16 +10,24 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 };
 
 
 export function Question ({
   content,
   author,
-  children
+  children,
+  isAnswered = false,
+  isHighlighted = false
 }: QuestionProps) {
   return (
-    <div className="question">
+    <div className={ classNames(
+      'question',
+      { answered: isAnswered },
+      { highlighted: isHighlighted && !isAnswered }
+    ) }>
       <p>{ content }</p>
       <footer>
         <div className="user-info">
